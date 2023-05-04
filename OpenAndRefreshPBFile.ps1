75420432353ee30ff03dcefe054ce78e7adbe71b
Rlany <132611224+Rlany@users.noreply.github.com>
2023-05-04 21:06:43 +0200

    Add files via upload

---
+++ OpenAndRefreshPBFile.ps1
@@ -0,0 +1,111 @@
+﻿Param(
+    $sourceWorkbook = "Demo.pbix",
+    $workbook = "Demo_New.pbix",
+    $closeWB = $true
+)
+
+$ErrorActionPreference = 'Stop'
+
+# add reference to automation functions
+. .\PBUIAutomation.ps1
+
+##################################################################################
+##        BODY
+##################################################################################
+
+
+$folder = $PSScriptRoot 
+$srcFile = $folder + "\" + $sourceWorkbook
+$tgtFile = $folder + "\" + $workbook
+
+#kill process if the workbook is opened
+#kill process if the workbook is opened and start new one
+#referenced from PBUIAutomation.ps1
+KillOldPB -targetWorkbook $tgtFile;
+
+
+#copy workbook under new name
+Copy-Item -Path $srcFile -Destination $tgtFile -force
+
+
+$workbookName = (get-item $tgtFile).BaseName
+$appWindowName = "$workbookName - Power BI Desktop"
+$refreshButtonName = "Refresh"
+$refreshWindowName = "Refresh"
+
+#Open Power BI workbook
+Invoke-Item $tgtFile
+ 
+#Wait for main window of Power BI Desktop  
+#referenced from PBUIAutomation.ps1 
+WaitForPBtoOpen -appWindowName $appWindowName
+
+# get app ID so we can check after refresh is hit running modal window with title refresh
+$appUI = FindProcessUI -appName $appWindowName;
+
+
+#Press REFRESH
+FindAndClickByName -name $refreshButtonName -appUI $appUI
+"Refreshing"
+
+Start-Sleep -Seconds 1
+
+# find refreshing window and wait until it is running
+$refreshOpen = $true
+while ($refreshOpen) {
+    try{
+        $refreshWindow = FindModalWindow -name $refreshWindowName -appUIObject $appUI
+        $refreshOpen = ($null -ne $refreshWindow);
+        #if refresh window is open then wait for 10 seconds and try again
+        if ($refreshOpen) {
+            Start-Sleep -Seconds 10
+            "*"
+        }
+    }
+    catch{
+        $refreshOpen = $false;
+    }
+}
+
+
+#Refreshing windows is not running anymore
+"Data refreshed locally"
+
+
+# save
+FindAndClickByName -name "Save" -appUI $appUI
+"File successfully saved"
+
+Start-Sleep -Seconds 3
+
+#close
+if ($closeWB) 
+{
+    FindAndClickByName -name "Close" -appUI $appUI
+    "Successfully closed"
+}
+<#
+this is code for playing around
+$cond5 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::NameProperty, "System Menu Bar")
+$menu = $appUI.FindFirst([Windows.Automation.TreeScope]::Descendants, $cond2)
+
+$cond4 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::IsEnabledProperty, $true)
+$appPartSaveAS = $menu.FindAll([Windows.Automation.TreeScope]::Descendants, $cond4)
+
+foreach($qq in $appPartSaveAS)
+{ 
+    $cn = $qq.Current.ClassName;
+    write-host $cn
+    $cc = $qq.Current;
+    write-host $cc;
+    write-host $cc.Name;
+    if($cc.Name -like "*menu*")
+    {
+        write-host $cc.Name;    
+    }
+    $ccs = $cc;
+}
+return;
+
+
+#>
---
+++ PBUIAutomation.ps1
@@ -0,0 +1,118 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+Add-Type –AssemblyName UIAutomationClient
+Add-Type –AssemblyName UIAutomationTypes
+
+##################################################################################
+##        Functions
+##################################################################################
+
+#------------------------------------------------------------------------------------------------
+# Find PowerBI Main app UI ID
+function FindProcessUI([string]$appName)
+{
+    $processId = ((Get-Process).where{$_.MainWindowTitle -eq $appName})[0].Id
+    $root = [Windows.Automation.AutomationElement]::RootElement
+    $condition = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::ProcessIdProperty, $processId)
+    $appUI = $root.FindFirst([Windows.Automation.TreeScope]::Children, $condition)
+    return $appUI
+}
+
+#------------------------------------------------------------------------------------------------
+# Find componet of application by class name and name of element
+function FindAppPart([string]$name, [string]$className, [System.Windows.Automation.AutomationElement]$appUI, [boolean]$useClassName = $true)
+{
+    $cond = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::NameProperty, $name)
+    if($useClassName)
+    {
+        $cond2 = New-Object Windows.Automation.PropertyCondition([Windows.Automation.AutomationElement]::ClassNameProperty, $className)
+        $cond3 = New-Object Windows.Automation.AndCondition($cond, $cond2)
+    }
+    else
+    {
+        $cond3 = $cond;
+    }
+    $appPart = $appUI.FindFirst([Windows.Automation.TreeScope]::Descendants, $cond3)
+   # $cn = $appPart.Current.ClassName;
+   # write-host $cn
+   # $cc = $appPart.Current;
+   # write-host $cc
+   # $ccs = $cc;
+    return $appPart;
+}
+
+#------------------------------------------------------------------------------------------------
+# Find button by name
+function FindButton([string]$name, [System.Windows.Automation.AutomationElement]$appUI){
+    
+	$button = FindAppPart -name $name -className "ms-Button root-199" -appUI $appUI
+    return $button 
+}
+
+#------------------------------------------------------------------------------------------------
+# Find modal winbdow by name
+function FindModalWindow([string]$name, $appUIObject){
+    
+	$window = FindAppPart -name $name -className "modal-frame" -appUI $appUIObject
+    return $window
+}
+
+#------------------------------------------------------------------------------------------------
+# Find button by name and invoke click
+function FindAndClickButton([string]$name, [System.Windows.Automation.AutomationElement]$appUI){
+	
+    $button = FindButton -name $name -appUI $appUI
+	$button.GetCurrentPattern([Windows.Automation.InvokePattern]::Pattern).Invoke()
+}
+
+#------------------------------------------------------------------------------------------------
+# Find object by name
+function FindByName([string]$name, [System.Windows.Automation.AutomationElement]$appUI){
+    
+	$button = FindAppPart -name $name -className "none" -appUI $appUI -useClassName $false
+    return $button 
+}
+
+
+# Find object by name and invoke click
+function FindAndClickByName([string]$name, [System.Windows.Automation.AutomationElement]$appUI){
+	
+    $button = FindByName -name $name -appUI $appUI
+	$button.GetCurrentPattern([Windows.Automation.InvokePattern]::Pattern).Invoke()
+}
+
+# This method kills PB if it's running already with existing workbook
+# and then runs PB with reference of the workbook
+function KillOldPB([string]$targetWorkbook){
+    
+    $workbookName = (get-item $targetWorkbook).BaseName
+    $appWindowName = "$workbookName - Power BI Desktop"
+    $processId = ((Get-Process).where{$_.MainWindowTitle -eq $appWindowName})[0].Id
+    
+    if($processId ){
+        "Power BI workbook $workbookName is already open. Killing process..."
+        taskkill /F /PID $processId
+        Start-Sleep -Seconds 2
+    }
+}
+
+# this method will keep sleeping until power bi deskto is open.
+# we identify this by finding app window and finding enabled refresh button.
+function WaitForPBtoOpen([string]$appWindowName){
+    $ready = $false;
+    "Waiting for Power BI to open"
+    #We are waiting until refresh button is ready and enabled - this mean mode is loaded
+    while (!$ready){
+        try{
+            $xAppUI = FindProcessUI -appName $appWindowName
+            $refreshButton = FindByName -name "Refresh" -appUI $xAppUI
+            $ready = !($null -eq $refreshButton)
+        }
+        catch{
+            Start-Sleep -Seconds 3
+            "*"
+        }
+    }
+    "Power BI opened"
+}
